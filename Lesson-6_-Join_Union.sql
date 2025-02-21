05a75a4cd8efbc070d8d1b05e229d2f89a044cb7
Dasha <dashakonopelko@gmail.com>
2025-02-21 19:51:14 +0200

    join_subqueries

---
+++ Lesson-6_-Join_Union.sql
@@ -0,0 +1,111 @@
+﻿-- JOIN operator - використовується для отримання зв'язаних записів з різних таблиць
+USE University;
+GO
+
+select s.Name, s.Email, s.GroupId, g.Name, g.Id, s.AverageMark
+from Students as s, Groups as g
+where g.Id = s.GroupId and AverageMark >= 10
+
+select * from Groups
+select * from Students
+
+select g.Name as [Group], COUNT(s.Id)
+from Groups as g JOIN Students as s ON s.GroupId = g.Id
+group by g.Name
+
+-- [INNER] JOIN - повертає всі записи таблиці A, які зв'язані з записами таблиці B
+-- показати всіх студентів та їх групи
+select s.Name, s.Email, s.AverageMark, g.Name as [Group]
+from Students as s INNER JOIN Groups as g ON s.GroupId = g.Id
+
+-- LEFT JOIN - повертає всі записи таблиці A, які зв'язані або не мають зв'язку з записами таблиці B
+-- показати всіх студентів та їх групи, а також тих, які не мають групи
+select s.Name, s.Email, s.AverageMark, g.Name as [Group]
+from Students as s LEFT JOIN Groups as g ON s.GroupId = g.Id
+
+-- показати всіх викладачів та к-сть їхніх груп, а також тих в кого немає жодної групи
+select t.Name, COUNT(g.Id)
+from Teachers as t LEFT JOIN TeachersGroups as tg ON tg.TeacherId = t.Id
+				   LEFT JOIN Groups as g ON tg.GroupId = g.Id
+group by t.Name, t.Id
+
+-- LEFT JOIN with NULL FOREIGN KEY - повертає лише записи таблиці A, які не зв'язані з жодним записом таблиці B
+-- показати лише студентів які не мають групи
+select s.Name, s.Email, s.AverageMark, g.Name as [Group]
+from Students as s LEFT JOIN Groups as g ON s.GroupId = g.Id
+where s.GroupId IS NULL
+
+-- RIGHT JOIN - повертає всі записи таблиці B, які зв'язані або не мають зв'язку з записами таблиці A
+-- показати всіх студентів та їх групи, а також ті групи, які не мають жодного студента
+insert into Groups
+values ('Ukraine')
+
+select s.Name, s.Email, s.AverageMark, g.Name as [Group]
+from Students as s RIGHT JOIN Groups as g ON s.GroupId = g.Id
+
+-- RIGHT JOIN with NULL FOREIGN KEY - повертає лише записи таблиці B, які не зв'язані з жодним записом таблиці A
+-- показати лише ті групи, які не мають жодного студента
+select s.Name, s.Email, s.AverageMark, g.Name as [Group]
+from Students as s RIGHT JOIN Groups as g ON s.GroupId = g.Id
+where s.GroupId IS NULL
+
+-- FULL [OUTER] JOIN - повертає записи таблиці A та B, які зв'язані або не мають зв'язку між собою
+-- показати всіх студентів, які мають групу або не мають її, а також групи, які не мають жодного студента
+select s.Name, s.Email, s.AverageMark, g.Name as [Group]
+from Students as s FULL OUTER JOIN Groups as g ON s.GroupId = g.Id
+
+-- FULL [OUTER] JOIN with NULL FOREIGN KEY - повертає лише записи таблиці A та B, які не мають зв'язку між собою
+-- показати лише студентів, які не мають групу, а також лише групи, які не мають жодного студента
+select s.Name, s.Email, s.AverageMark, g.Name as [Group]
+from Students as s FULL OUTER JOIN Groups as g ON s.GroupId = g.Id
+where s.GroupId IS NULL
+
+
+
+
+-- UNION - об'єднує декілька запитів в одну результуючу таблицю
+--		   при цьому видаляючи дублікати
+
+/*
+select ...
+UNION [ALL]
+select ...
+*/
+
+select * from Students
+
+-- показати кількість всіх студентів та їх середній бал
+select 'Students Count', COUNT(Id)
+from Students
+union
+select 'Average Mark', AVG(AverageMark)
+from Students
+
+-- показати імена студентів та викладачів
+select  Name
+from Students
+union
+select  Name
+from Teachers
+
+-- показати імена студентів та викладачів, які починаються на літеру 'V'
+select Id, Name
+from Students
+where Name LIKE '[C]%'
+--order by Name DESC -- помилка
+UNION
+select Id, Name
+from Teachers
+where Name LIKE '[C]%'
+order by Name ASC -- сортування об'єднаних запитів дозволяється лише вкінці
+
+-- UNION ALL - об'єднує декілька запитів в одну результуючу таблицю
+--			   при цьому дублікати не видаляються
+select Id, Name, AverageMark
+from Students
+where AverageMark >= 10
+UNION all
+select Id, Name, AverageMark
+from Students
+where AverageMark >= 7
+order by Name
---
+++ Lesson-6_-Subqueries.sql
@@ -0,0 +1,165 @@
+﻿use UniversityPB_421
+
+-----------------------------
+-- Inner Queries (Subqueries)
+
+select * from Students;
+select * from Groups;
+
+-- запит, який повертає максимальну оцінку по всіх студентах
+select Max(AverageMark)
+from Students
+
+-- отримуємо всіх студентів, які мають максимальну оцінку
+select *
+from Students
+where AverageMark = (
+			select Max(AverageMark)
+			from Students
+)
+-- отримуємо всі групи, які мають максимальну кі-сть студентів
+
+select g.Name, AVG(s.AverageMark), COUNT(s.Id)
+from Students as s join Groups as g on s.GroupId=g.Id
+group by g.Name
+having COUNT(s.Id) = (
+			select top 1 COUNT(s.Id)
+			from Students as s join Groups as g on g.Id = s.GroupId
+			group by g.Name
+			order by COUNT(s.Id) desc
+			)
+
+-- отримуємо всі групи, які мають максимальну або мінімальну кі-сть студентів
+select g.Name, AVG(s.AverageMark), COUNT(s.Id)
+from Students as s join Groups as g on s.GroupId=g.Id
+group by g.Name
+having COUNT(s.Id) = (
+			select top 1 COUNT(s.Id)
+			from Students as s join Groups as g on g.Id = s.GroupId
+			group by g.Name
+			order by COUNT(s.Id) desc
+			)
+		or
+			COUNT(s.Id) = (
+			select top 1 COUNT(s.Id)
+			from Students as s join Groups as g on g.Id = s.GroupId
+			group by g.Name
+			order by COUNT(s.Id) 
+			)
+
+-- якщо запит повертає декілька значень, для перевірки можна використати оператор IN
+--in (1,2,3)
+select Id, Name, Birthdate
+from Students
+where Id in (select Id
+	from Students
+	where Name like 'C%')
+
+-- показати всіх викладачів, в яких більша половина студентів мають відмінні оцінки
+
+select t2.Id, t2.Name as [Teacher], Avg(s2.AverageMark) as [Mark], Count(s2.Id) as [Students] 
+from Teachers as t2 join TeachersGroups as tg2 on t2.Id = tg2.TeacherId
+					join Groups as g2 on g2.Id = tg2.GroupId
+					join Students as s2 on s2.GroupId = g2.Id 
+group by t2.Name, t2.Id
+having COUNT(s2.Id) / 2 < (
+					select Count(s.Id)
+						from Students as s join Groups as g on s.GroupId = g.Id
+						join TeachersGroups as tg on g.Id = tg.GroupId
+						join Teachers as t on tg.TeacherId = t.Id
+						where s.AverageMark >= 7 and t.Id = t2.Id
+)
+
+
+
+
+/* Subquery Operators
+	- [NOT] EXISTS				  - повертає TRUE якщо запит повернув хоча б один запис
+	- [> < >= <= <> =] ANY / SOME - повертає TRUE якщо хоча б одни запис відповідає умові
+	- [> < >= <= <> =] ALL		  - повертає TRUE якщо всі записи відповідають умові
+*/
+
+
+
+/*
+...
+where/having [NOT] EXISTS (query)
+...
+*/
+select* from Teachers
+select * from TeachersGroups
+-- показати викладічів, які мають хоча б одну групу
+select Id,Name, Phone
+from Teachers as t
+where EXISTS ( select * from TeachersGroups as tg 
+					where tg.TeacherId = t.Id)
+
+-- показати групи, які мають хоча б одного студента з оцінкою 12
+select Name
+from Groups
+where EXISTS (select Id from Students where AverageMark = 11.2 and GroupId = Groups.Id)
+
+select Name from Students where AverageMark = 11.2
+
+-- показати викладачів, які мають хоча б одного студента з іменем 'Sophi'
+select Name, Phone
+from Teachers
+where Exists (
+				select s.Id
+				from Students as s join Groups as g on g.Id = s.GroupId
+				join TeachersGroups as tg on g.Id = tg.GroupId
+				where tg.TeacherId = Teachers.Id and s.Name = 'Sophi')
+select *
+from Students
+where Name = 'Sophi'
+-- показати групи в яких є хоча б один студент старше 20-ти
+select Name
+from Groups as g
+where EXISTS (select Id
+			  from Students as s
+			  where DATEDIFF(YEAR, s.BirthDate, GETDATE()) >= 20 AND s.GroupId = g.Id);
+
+/*
+...
+where/having [> < >= <= <> =] ANY/SOME / ALL (query)
+...
+*/
+-- показати студентів, в яких ім'я співпадає з іменем якогось викладача
+select * from Teachers
+insert into Teachers
+values ('Sophi','2000/02/02','+3805236589')
+
+select Name, Birthdate, Email
+from Students
+where Name = Some(select Name from Teachers)
+-- показати студентів, в яких дата народження більша за дату прийняття на роботу будь-якого викладача
+select Name, Birthdate, Email
+from Students
+where Birthdate > Any(select HireDate from Teachers)
+
+-- показати студентів з ім'ям яке має хоча б одни студент іншої групи
+select * from Students
+insert into Students
+values ('Sophi', 'Tarasenko', 'sophi@gmail.com','2005/03/03',default, 200, default, 1)
+
+select Name, Birthdate, Email
+from Students
+where Name = Any(select Name from Students as s where s.GroupId <> Students.GroupId)
+
+-- показати студентів в яких оцінка більша за оцінки всіх студентів групи 'New-York'
+select s.Name, s.Email, s.AverageMark, g.Name
+from Students as s join Groups as g on s.GroupId = g.Id
+where AverageMark > ALL (select AverageMark 
+							from Students as s2 join Groups as g2 on s2.GroupId = g2.Id
+							where g2.Name ='New-York')
+
+-- показати викладачів які були прийняті на роботу раніше дати народження всіх їхніх студентів
+select Name, Phone, Hiredate
+from Teachers
+where HireDate < ALL(
+						select s.Birthdate
+						from Students as s join Groups as g on s.GroupId = g.Id
+											join TeachersGroups as tg on tg.GroupId = g.Id
+						where tg.TeacherId = Teachers.Id)
+
+
